3309b2f015572c7d9e5f8ba55664dbd1a5b3049a
yijerjer <loh.yijer@live.com>
2017-04-24 21:24:24 +0800

    added action_cable route and dev config

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -2,8 +2,4 @@ class ChatChannel < ApplicationCable::Channel
   def subscribed
     stream_from "match_#{params[:match_id]}"
   end
-
-  def receive(data)
-    ActionCable.server.broadcast("match_#{params[:match_id]}", data)
-  end
 end
